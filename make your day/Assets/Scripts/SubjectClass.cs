014dc378f3289bea41c5687ea62d34bc5573c32a
stayup24h <sani0501we@gmail.com>
2024-05-24 15:26:17 +0900

    ✨ feat: make Subject.cs that manage subject's name, color, score

---
+++ make your day/Assets/Scripts/SubjectClass.cs
@@ -0,0 +1,23 @@
+public class Subject
+{
+    public string name;
+    public string color;
+    public int usedCard;
+
+    public Subject(string _name, string _color)
+    {
+    name = _name;
+    color = _color;
+    usedCard = 0;
+    }
+
+    public void Reset()
+    {
+        usedCard = 0;
+    }
+
+    public void UseCard()
+    {
+        usedCard += 1;
+    }
+}
